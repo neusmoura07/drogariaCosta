4eef085a0cec972cadefae1bd39e5dbdafeac809
Neus Moura <96215404+neusmoura07@users.noreply.github.com>
2024-04-22 20:08:56 -0300

    2 commit
    Header feito e categorias abrindo e fechando

---
+++ Site/js/script.js
@@ -0,0 +1,16 @@
+document.getElementById('menuToggle').addEventListener('click', function() {
+    document.getElementById('menu').classList.toggle('open');
+}); /* FAZ COM QUE O MENU SEJA ABERTO*/
+
+document.getElementById('menuToggle').addEventListener('click', function() {
+    this.classList.toggle('open'); // Adiciona ou remove a classe "open" ao botão de alternância
+}); /* FAZ A ANIMAÇÃO DO X FUNCIONAR */
+
+document.getElementById('menuToggle').addEventListener('click', function() {
+    var menuContainer = document.querySelector('.menu-container');
+    if (menuContainer.classList.contains('open')) {
+        menuContainer.classList.remove('open');
+    } else {
+        menuContainer.classList.add('open');
+    }
+}); /* FORÇA O MENU ABRIR */
