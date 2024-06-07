91adbe4391aadbcab5a04ed956feaf7a65de9115
Neus Moura <96215404+neusmoura07@users.noreply.github.com>
2024-06-07 17:35:25 -0300

    Adição do Carrossel da página Inicial
    Foi feito o carrossel da primeira tela. precisa de ajustes mas o mais complexo foi feito

--- js/script.js
+++ js/script.js
@@ -15,3 +15,44 @@ document.getElementById('menuToggle').addEventListener('click', function() {
     }
 }); /* FORÇA O MENU ABRIR */
 
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('#carouselProducts', {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
+// Ajusta a resolução do carrossel
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: $(".owl-carousel").data('items'),
+        loop: true,
+        margin: [0, 10, 20, 30, 40], // Valores de margem para cada ponto de quebra
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                margin: -60, // Margem zero para dispositivos com menos de 576 pixels de largura
+            },
+            576: {
+                items: 2,
+                margin: 10, // Margem de 10 pixels para dispositivos com largura entre 576px e 767px
+            },
+            768: {
+                items: 3,
+                margin: 20, // Margem de 20 pixels para dispositivos com largura entre 768px e 991px
+            },
+            992: {
+                items: 4,
+                margin: 30, // Margem de 30 pixels para dispositivos com largura entre 992px e 1199px
+            },
+            1200: {
+                items: 5,
+                margin: 40, // Margem de 40 pixels para dispositivos com largura maior ou igual a 1200px
+            }
+        }
+    });
+});
